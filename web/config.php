53fd27fb7778dc9c5620e7c208ea77006ee93112
mayunbaba2 <mayunbaba2@users.noreply.github.com>
2018-02-04 20:48:46 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyAmJ_XU5E88qdy_r38nl641o8oZYA1CDzU"));
+define('GJ_CODE', getenv("HK"));
+define('SITE_NAME',getenv("YouTube"));
+define('TITLENAME', getenv("Tube"));
+define('EN2DEKEY',getenv("grtowfj&83*@k&^8"));
+define('EMAIL', getenv("y2b@heroku.com"));
 ?>
